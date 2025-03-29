21ba950008a60f049a02d3f501c566448e4e4481
PoorInRichfield <25310942+ToddTaylor@users.noreply.github.com>
2025-03-29 17:30:41 -0500

    Added subscriber configuration

--- RailroadTelemetryLogService.ConsoleApp/Program.cs
+++ RailroadTelemetryLogService.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ConsoleApp.Deserializers;
 using ConsoleApp.EventArgs;
-using RailroadTelemetryLogService.ConsoleApp;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 class Program
 {
@@ -10,8 +11,15 @@ class Program
 
     static void Main()
     {
-        HotEotPacketSubscriber subscriber = new HotEotPacketSubscriber();
-        HotPacketReceived += OnHotPacketReceived;
+        var configuration = LoadConfiguration();
+
+        // Add subscribers to the HotPacketReceived event
+        var subscriberTypes = configuration.GetSection("HotPacketSubscription:Subscribers").Get<string[]>();
+
+        foreach (var subscriberType in subscriberTypes)
+        {
+            SubscribeToHotPacketEvent(subscriberType);
+        }
 
         var logFilePath = $@"C:\TrainDetection\Logs\eot{GetTodayDateString()}.log";
         var lastPosition = 0L;
@@ -32,14 +40,13 @@ class Program
 
                     Console.WriteLine(line);
 
-                    if (line.StartsWith("#"))
+                    if (IsHeaderRow(line))
                     {
                         continue;
                     }
 
                     var hotPacket = HotEotDeserializer.Deserialize(line);
 
-                    // Raise the event
                     HotPacketReceived?.Invoke(null, new HotPacketEventArgs(hotPacket));
                 }
 
@@ -54,13 +61,46 @@ class Program
         }
     }
 
-    static string GetTodayDateString()
+    private static bool IsHeaderRow(string? line)
+    {
+        return line.StartsWith("#");
+    }
+
+    private static IConfiguration LoadConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        return builder.Build();
+    }
+
+    private static void SubscribeToHotPacketEvent(string subscriberTypeName)
+    {
+        var type = Type.GetType(subscriberTypeName);
+        if (type != null)
+        {
+            var subscriber = Activator.CreateInstance(type);
+            if (subscriber != null)
+            {
+                var eventInfo = typeof(Program).GetEvent("HotPacketReceived");
+                var methodInfo = type.GetMethod("OnHotPacketReceived", BindingFlags.NonPublic | BindingFlags.Instance);
+                if (eventInfo != null && methodInfo != null)
+                {
+                    var handler = Delegate.CreateDelegate(eventInfo.EventHandlerType, subscriber, methodInfo);
+                    eventInfo.AddEventHandler(null, handler);
+                }
+            }
+        }
+    }
+
+    private static string GetTodayDateString()
     {
         return DateTime.Now.ToString("yyyyMMdd");
     }
 
-    static void OnHotPacketReceived(object sender, HotPacketEventArgs e)
+    private static void OnHotPacketReceived(object sender, HotPacketEventArgs e)
     {
-        //Console.WriteLine($"HotPacket received: {e.HotPacket}");
+        // Event subscribers will implement this method.
     }
 }
