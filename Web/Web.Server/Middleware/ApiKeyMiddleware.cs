bf090b443275b432876efd9036bc5e08169ddf97
PoorInRichfield <25310942+ToddTaylor@users.noreply.github.com>
2025-05-07 15:46:26 -0500

    Enabled header API key middleware

---
+++ Web/Web.Server/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,41 @@
+﻿namespace Web.Server.Middleware
+{
+    public class ApiKeyMiddleware
+    {
+        private const string ApiKeyHeaderName = "X-Api-Key";
+        private readonly RequestDelegate _next;
+        private readonly string _apiKey;
+
+        public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _apiKey = configuration["ApplicationSettings:ApiKey"];
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Bypass Swagger endpoints
+            if (context.Request.Path.StartsWithSegments("/swagger"))
+            {
+                await _next(context);
+                return;
+            }
+
+            if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("API Key is missing.");
+                return;
+            }
+
+            if (!_apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsync("Invalid API Key.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
