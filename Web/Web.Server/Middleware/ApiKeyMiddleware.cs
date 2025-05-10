c9f1a94e8a0ee4e7e09eb8245a6d699e8e95710f
PoorInRichfield <25310942+ToddTaylor@users.noreply.github.com>
2025-05-10 14:46:23 -0500

    Modified API Key middleware to have a whitelist instead of blacklist.

--- Web/Web.Server/Middleware/ApiKeyMiddleware.cs
+++ Web/Web.Server/Middleware/ApiKeyMiddleware.cs
@@ -14,29 +14,26 @@
 
         public async Task InvokeAsync(HttpContext context)
         {
-            // Bypass Swagger and notification hub endpoints
-            if (context.Request.Path.StartsWithSegments("/swagger")
-                || context.Request.Path.StartsWithSegments("/hubs")
-                || context.Request.Path.StartsWithSegments("/"))
+            // Apply API key validation only for "/swagger" and "api/v1/[controller]" endpoints
+            if (context.Request.Path.StartsWithSegments("/swagger") ||
+                context.Request.Path.StartsWithSegments("/api/v1"))
             {
-                await _next(context);
-                return;
+                if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("API Key is missing.");
+                    return;
+                }
+
+                if (!_apiKey.Equals(extractedApiKey))
+                {
+                    context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    await context.Response.WriteAsync("Invalid API Key.");
+                    return;
+                }
             }
 
-            if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
-            {
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                await context.Response.WriteAsync("API Key is missing.");
-                return;
-            }
-
-            if (!_apiKey.Equals(extractedApiKey))
-            {
-                context.Response.StatusCode = StatusCodes.Status403Forbidden;
-                await context.Response.WriteAsync("Invalid API Key.");
-                return;
-            }
-
+            // Allow other requests to proceed without API key validation
             await _next(context);
         }
     }
