e2cb7e41f1a7f30d5b3bebb66f368171796badaf
PoorInRichfield <25310942+ToddTaylor@users.noreply.github.com>
2025-05-11 19:40:16 -0500

    Updated API middleware again to only include /api

--- Web/Web.Server/Middleware/ApiKeyMiddleware.cs
+++ Web/Web.Server/Middleware/ApiKeyMiddleware.cs
@@ -15,26 +15,21 @@
         public async Task InvokeAsync(HttpContext context)
         {
             // Bypass Swagger and notification hub endpoints
-            if (context.Request.Path.StartsWithSegments("/swagger")
-                || context.Request.Path.StartsWithSegments("/hubs")
-                || context.Request.Path.StartsWithSegments("/"))
+            if (context.Request.Path.StartsWithSegments("/api"))
             {
-                await _next(context);
-                return;
-            }
+                if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("API Key is missing.");
+                    return;
+                }
 
-            if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
-            {
-                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                await context.Response.WriteAsync("API Key is missing.");
-                return;
-            }
-
-            if (!_apiKey.Equals(extractedApiKey))
-            {
-                context.Response.StatusCode = StatusCodes.Status403Forbidden;
-                await context.Response.WriteAsync("Invalid API Key.");
-                return;
+                if (!_apiKey.Equals(extractedApiKey))
+                {
+                    context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    await context.Response.WriteAsync("Invalid API Key.");
+                    return;
+                }
             }
 
             await _next(context);
