49d366360dac1d3fac75fae55da401c87341a024
PoorInRichfield <25310942+ToddTaylor@users.noreply.github.com>
2025-05-17 17:33:47 -0500

    Added comments to CreateTelemetryDTO

--- Web/Web.Server/DTOs/CreateTelemetryDTO.cs
+++ Web/Web.Server/DTOs/CreateTelemetryDTO.cs
@@ -2,17 +2,31 @@
 {
     public class CreateTelemetryDTO : IEquatable<CreateTelemetryDTO?>
     {
+        /// <summary>
+        /// The ID of the beacon that sent the telemetry.
+        /// </summary>
+        /// <remarks>The beacon ID must be obtained from the API administrator.</remarks>
         public required int BeaconID { get; set; }
 
+        /// <summary>
+        /// The Address ID of the telemetry from SoftEOT's ID value or 
+        /// SoftDPU's ADDR value.
+        /// </summary>
         public required int AddressID { get; set; }
 
+        /// <summary>
+        /// Optional: The Train ID of the telemetry from SoftDPU's TRID value.
+        /// </summary>
         public int? TrainID { get; set; }
 
+        /// <summary>
+        /// Optional: The moving status of the train from SoftEOT's MOT value.
+        /// </summary>
         public bool? Moving { get; set; }
 
         /// <summary>
-        /// The source of the alert.
-        /// HOT, EOT, DPU, HBD
+        /// The source system of the telemetry.
+        /// Valid values include: HOT, EOT, DPU, HBD
         /// </summary>
         public required string Source { get; set; }
 
