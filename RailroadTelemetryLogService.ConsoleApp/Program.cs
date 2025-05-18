6523047cd4627545d2a7ef4a041982cebff7394d
PoorInRichfield <25310942+ToddTaylor@users.noreply.github.com>
2025-05-18 15:23:45 -0500

    Refactored console app to only process messages less than 5 minutes old

--- RailroadTelemetryLogService.ConsoleApp/Program.cs
+++ RailroadTelemetryLogService.ConsoleApp/Program.cs
@@ -5,8 +5,12 @@ using System.Reflection;
 
 class Program
 {
-    public delegate void DpuPacketEventHandler(object sender, DpuPacketEventArgs e);
-    public delegate void HotEotPacketEventHandler(object sender, HotEotPacketEventArgs e);
+    private const int TIME_RECEIVED_OFFSET_MINUTES = 5;
+    private const string FILE_NAME_PREFIX_EOT = "eot";
+    private const string FILE_NAME_PREFIX_DPU = "dpu";
+
+    public delegate void DpuPacketEventHandler(object? sender, DpuPacketEventArgs e);
+    public delegate void HotEotPacketEventHandler(object? sender, HotEotPacketEventArgs e);
     public static event DpuPacketEventHandler DpuPacketReceived;
     public static event HotEotPacketEventHandler HotEotPacketReceived;
 
@@ -17,6 +21,12 @@ class Program
         // Add subscribers to the events
         var hotEotSubscriberTypes = configuration.GetSection("HotEotPacketSubscription:Subscribers").Get<string[]>();
         var dotSubscriberTypes = configuration.GetSection("DpuPacketSubscription:Subscribers").Get<string[]>();
+
+        if (hotEotSubscriberTypes == null || dotSubscriberTypes == null)
+        {
+            LogError("No subscribers found in the configuration.");
+            return;
+        }
 
         foreach (var subscriberType in hotEotSubscriberTypes)
         {
@@ -29,59 +39,100 @@ class Program
         }
 
         var logDirectoryPath = configuration.GetValue<string>("LogDirectoryPath");
+
+        if (string.IsNullOrEmpty(logDirectoryPath) || !Directory.Exists(logDirectoryPath))
+        {
+            LogError("Log directory path is not valid.");
+            return;
+        }
+
         var lastPositions = new Dictionary<string, long>();
+
+        Console.WriteLine($"Telemetry Log Service started.  Processing messages posted with then last {TIME_RECEIVED_OFFSET_MINUTES} minutes...");
 
         while (true)
         {
             try
             {
-                var logFilePaths = Directory.GetFiles(logDirectoryPath, $"*{GetTodayDateString()}.log");
+                // Get EOT and DPU log files for today
+                var todaysLogFilePaths = Directory.GetFiles(logDirectoryPath, $"*{GetTodayDateString()}.log");
 
-                foreach (var logFilePath in logFilePaths)
-                {
-                    if (!lastPositions.ContainsKey(logFilePath))
-                    {
-                        lastPositions[logFilePath] = 0L;
-                    }
-
-                    using var fileStream = new FileStream(logFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                    fileStream.Seek(lastPositions[logFilePath], SeekOrigin.Begin);
-
-                    using var reader = new StreamReader(fileStream);
-
-                    while (!reader.EndOfStream)
-                    {
-                        var line = reader.ReadLine();
-
-                        if (IsHeaderRow(line))
-                        {
-                            continue;
-                        }
-
-                        Console.WriteLine(line);
-
-                        if (Path.GetFileName(logFilePath).StartsWith("eot", StringComparison.OrdinalIgnoreCase))
-                        {
-                            var hotPacket = HotEotDeserializer.Deserialize(line);
-                            HotEotPacketReceived?.Invoke(null, new HotEotPacketEventArgs(hotPacket));
-                        }
-
-                        if (Path.GetFileName(logFilePath).StartsWith("dpu", StringComparison.OrdinalIgnoreCase))
-                        {
-                            var dpuPacket = DpuDeserializer.Deserialize(line);
-                            DpuPacketReceived?.Invoke(null, new DpuPacketEventArgs(dpuPacket));
-                        }
-
-                        lastPositions[logFilePath] = fileStream.Position;
-                    }
-                }
+                ProcessLogFiles(lastPositions, todaysLogFilePaths);
             }
             catch (IOException ex)
             {
-                Console.WriteLine("Error reading the file: " + ex.Message);
+                LogError("Error reading the file: " + ex.Message);
             }
 
             Thread.Sleep(1000); // Wait before checking the files again.
+        }
+    }
+
+    private static void LogError(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Error.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    private static void ProcessLogFiles(Dictionary<string, long> lastPositions, string[] logFilePaths)
+    {
+        foreach (var logFilePath in logFilePaths)
+        {
+            ProcessLogFile(lastPositions, logFilePath);
+        }
+    }
+
+    private static void ProcessLogFile(Dictionary<string, long> lastPositions, string logFilePath)
+    {
+        if (!lastPositions.ContainsKey(logFilePath))
+        {
+            lastPositions[logFilePath] = 0L;
+        }
+
+        using FileStream fileStream = new(logFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        fileStream.Seek(lastPositions[logFilePath], SeekOrigin.Begin);
+
+        using StreamReader reader = new(fileStream);
+
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+
+            if (IsHeaderRow(line))
+            {
+                continue;
+            }
+
+            if (Path.GetFileName(logFilePath).StartsWith(FILE_NAME_PREFIX_EOT, StringComparison.OrdinalIgnoreCase))
+            {
+                var hotPacket = HotEotDeserializer.Deserialize(line);
+
+                if (hotPacket.TimeReceived <= DateTime.Now.AddMinutes(-TIME_RECEIVED_OFFSET_MINUTES))
+                {
+                    continue; // Ignore packets older than 5 minutes
+                }
+
+                Console.WriteLine(line);
+
+                HotEotPacketReceived?.Invoke(null, new HotEotPacketEventArgs(hotPacket));
+            }
+
+            if (Path.GetFileName(logFilePath).StartsWith(FILE_NAME_PREFIX_DPU, StringComparison.OrdinalIgnoreCase))
+            {
+                var dpuPacket = DpuDeserializer.Deserialize(line);
+
+                if (dpuPacket.TimeReceived <= DateTime.Now.AddMinutes(-TIME_RECEIVED_OFFSET_MINUTES))
+                {
+                    continue; // Ignore packets older than 5 minutes
+                }
+
+                Console.WriteLine(line);
+
+                DpuPacketReceived?.Invoke(null, new DpuPacketEventArgs(dpuPacket));
+            }
+
+            lastPositions[logFilePath] = fileStream.Position;
         }
     }
 
