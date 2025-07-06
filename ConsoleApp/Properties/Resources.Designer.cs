3a5173dda4070441032e8023f7e681910fbf372a
PoorInRichfield <25310942+ToddTaylor@users.noreply.github.com>
2025-07-06 12:10:53 -0500

    Solution clean up

--- ConsoleApp/Properties/Resources.Designer.cs
+++ ConsoleApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RailroadTelemetry.ConsoleApp.Properties {
+namespace ConsoleApp.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace RailroadTelemetry.ConsoleApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RailroadTelemetry.ConsoleApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ConsoleApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
